928145b4c9f594e9fc2b10056d3b15d8eea5c485
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:51:43 -0300

    aula7

--- pratica/pratica07/main.java
+++ pratica/pratica07/main.java
@@ -1,5 +1,11 @@
-package pratica.pratica07;
+package aulas_pr.pratica7;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+    }
 }
