131ce344347f5bfa2489d5f47783610dbcbd7b42
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:54:41 -0300

    aula10

--- pratica/pratica10/main.java
+++ pratica/pratica10/main.java
@@ -1,5 +1,16 @@
-package pratica.pratica10;
+package aulas_pr.pratica10;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                soma = soma + i;
+            }
+        }
+
+        System.out.println("A soma dos números pares é: " + soma);
+    }
 }
