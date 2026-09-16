f07966adc0886dd8a6ba20c5d061c6209bc1593c
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:48:12 -0300

    aula4

--- pratica/pratica04/main.java
+++ pratica/pratica04/main.java
@@ -1,5 +1,17 @@
-package pratica.pratica04;
+package aulas_pr.pratica4;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 25;
+        int numero3 = 15;
+
+        if (numero1 > numero2 && numero1 > numero3) {
+            System.out.println("O maior número é: " + numero1);
+        } else if (numero2 > numero1 && numero2 > numero3) {
+            System.out.println("O maior número é: " + numero2);
+        } else {
+            System.out.println("O maior número é: " + numero3);
+        }
+    }
 }
