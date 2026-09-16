9f0754b8e9b6e2a42bb7203132ebcf1dc8e20e8c
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:53:38 -0300

    aula9

--- pratica/pratica09/main.java
+++ pratica/pratica09/main.java
@@ -1,5 +1,13 @@
-package pratica.pratica09;
+package aulas_pr.pratica9;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            soma += i;
+        }
+
+        System.out.println("A soma de 1 a 100 e: " + soma);
+    }
 }
