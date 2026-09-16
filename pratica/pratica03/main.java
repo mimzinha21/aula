3f6ab431fdb5b97c8dd4db6a2bd8436807c90939
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:46:31 -0300

    aula3

--- pratica/pratica03/main.java
+++ pratica/pratica03/main.java
@@ -1,3 +1,15 @@
-public class main {
-    
+package aulas_pr.pratica3;
+
+public class Main {
+    public static void main(String[] args) {
+        int nota = 5;
+
+        if (nota >= 6) {
+            System.out.println("Aprovado");
+        } else if (nota >= 4) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
 }
