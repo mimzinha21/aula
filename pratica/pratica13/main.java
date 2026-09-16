8e816a2c36884fcca1a9a012f2f56412f68cbeeb
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:57:41 -0300

    aula13

--- pratica/pratica13/main.java
+++ pratica/pratica13/main.java
@@ -1,5 +1,11 @@
-package pratica.pratica13;
+package aulas_pr.pratica13;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+        System.out.println("Primeiro elemento: " + numeros[0]);
+        System.out.println("Último elemento: " + numeros[9]);
+    }
 }
