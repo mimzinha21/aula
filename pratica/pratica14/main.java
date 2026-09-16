c149589f9c258cae9e91776ff556fdc941d1ae17
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:58:49 -0300

    aula14

--- pratica/pratica14/main.java
+++ pratica/pratica14/main.java
@@ -1,5 +1,18 @@
-package pratica.pratica14;
+package aulas_pr.pratica14;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeros = {15, 8, 32, 4, 19, 27};
+
+        int maior = numeros[0];
+
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+        }
+
+        System.out.println("Maior elemento: " + maior);
+    }
 }
