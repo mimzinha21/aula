fe297f8105409c9952074a4454e200e190f4cfbf
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:50:29 -0300

    aula6

--- pratica/pratica06/main.java
+++ pratica/pratica06/main.java
@@ -1,5 +1,13 @@
-package pratica.pratica06;
+package aulas_pr.pratica6;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
 }
