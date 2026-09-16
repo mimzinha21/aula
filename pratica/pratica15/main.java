2fc29be43519b5018b6f436bf86391c37e62392d
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 19:59:41 -0300

    aula15

--- pratica/pratica15/main.java
+++ pratica/pratica15/main.java
@@ -1,5 +1,18 @@
-package pratica.pratica15;
+package aulas_pr.pratica15;
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+
+        double[] notas = {7.5, 8.0, 6.5, 9.0, 5.5};
+
+        double soma = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            soma += notas[i];
+        }
+
+        double media = soma / notas.length;
+
+        System.out.println("Média: " + media);
+    }
 }
