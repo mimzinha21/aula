3124558dc312fce457a26baeadd5fb246fdad229
mimzinha21 <iasmimlaurindo8@gmail.com>
2026-09-16 22:17:42 -0300

    aulas

---
+++ 7_funcoes/exemplo3/main.java
@@ -0,0 +1,8 @@
+public class Main{
+    static boolean ligarInterruptor(){
+        return true;
+    }
+    public static void main(String[] args){
+        system.out.println(LigarInterruptor());
+    }
+}
---
+++ 7_funcoes/exercicio1/main.java
@@ -0,0 +1,9 @@
+public class Main{
+    static void analisar(int num){
+        if (num>0)
+
+        system.out.println("Eh maior que zero");
+        if(num<0)
+
+    }
+}
